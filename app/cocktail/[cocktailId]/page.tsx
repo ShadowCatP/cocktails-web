ae2b482ae574f02d7bc24c5400cde60af437ea85
ShadowCatP <282276@student.pwr.edu.pl>
2025-03-15 17:19:55 +0100

    [feature] made a single cocktail page

--- app/cocktail/[cocktailId]/page.tsx
+++ app/cocktail/[cocktailId]/page.tsx
@@ -1,11 +1,25 @@
 "use client";
 
+import { IngredientCard } from "@/components/ui/Cards/IngredientCard";
 import { useCocktailById } from "@/hooks/useCocktailById";
 import { useParams, notFound } from "next/navigation";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import Link from "next/link";
+import { faList, faMartiniGlass } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useRouter } from "next/navigation";
 
 export default function CocktailPage() {
   const { cocktailId } = useParams();
   const { data, isLoading } = useCocktailById(Number(cocktailId));
+  const router = useRouter();
 
   if (isLoading) return <main>Loading...</main>;
 
@@ -17,8 +31,56 @@ export default function CocktailPage() {
     data.data;
 
   return (
-    <main>
-      <h2>{name}</h2>
+    <main className="mx-auto my-6 flex w-95/100 max-w-[1024px] flex-col gap-6 rounded-4xl bg-gray-200 p-6">
+      <div>
+        <Breadcrumb>
+          <BreadcrumbList>
+            <BreadcrumbItem>
+              <BreadcrumbLink asChild>
+                <button onClick={() => router.back()}>Cocktails</button>
+              </BreadcrumbLink>
+            </BreadcrumbItem>
+            <BreadcrumbSeparator />
+            <BreadcrumbItem>
+              <BreadcrumbPage>{name}</BreadcrumbPage>
+            </BreadcrumbItem>
+          </BreadcrumbList>
+        </Breadcrumb>
+      </div>
+
+      <div className="flex flex-col gap-3 md:flex-row">
+        <div>
+          <img src={imageUrl} alt={name} className="rounded-2xl" />
+        </div>
+        <div className="flex flex-col gap-3">
+          <div className="flex flex-col gap-3">
+            <h2 className="text-3xl font-bold tracking-wider">{name}</h2>
+            <div className="flex gap-3">
+              <span className="rounded-xl bg-purple-500 px-3 py-1 text-white">
+                <FontAwesomeIcon className="mr-2" icon={faList} />
+                {category}
+              </span>
+              <span className="rounded-xl bg-blue-500 px-2 py-1 text-white">
+                <FontAwesomeIcon className="mr-2" icon={faMartiniGlass} />
+                {glass}
+              </span>
+            </div>
+          </div>
+          <div className="flex flex-col">
+            <h3 className="text-2xl font-medium">Intructions</h3>
+            <p>{instructions}</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex flex-col gap-3">
+        <h3 className="text-2xl font-medium">Ingredients</h3>
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+          {ingredients?.map((ingredient) => (
+            <IngredientCard ingredient={ingredient} key={ingredient.id} />
+          ))}
+        </div>
+      </div>
     </main>
   );
 }
---
+++ components/ui/Cards/IngredientCard.tsx
@@ -0,0 +1,43 @@
+import { Ingredient } from "@/api/api.types";
+import { HTMLAttributes } from "react";
+
+interface IngredientCardProps extends HTMLAttributes<HTMLDivElement> {
+  ingredient: Ingredient;
+}
+
+export const IngredientCard = ({
+  ingredient,
+  className,
+  ...rest
+}: IngredientCardProps) => {
+  return (
+    <div
+      className={`flex gap-6 rounded-lg bg-gray-100 p-4 transition-transform hover:scale-105 ${className}`}
+      {...rest}
+    >
+      <div className="relative mr-0 flex-shrink-0">
+        {ingredient.imageUrl && (
+          <img
+            src={ingredient.imageUrl}
+            alt={ingredient.name}
+            className="h-24 w-24 rounded-full border-2 border-gray-200 object-cover p-2"
+          />
+        )}
+        {(ingredient.alcohol, ingredient.percentage) && (
+          <span
+            className="absolute right-0 bottom-0 grid h-8 w-8 place-content-center rounded-full bg-red-500 text-xs text-white"
+            title={`${ingredient.percentage}% alcohol`}
+          >
+            {ingredient.percentage}%
+          </span>
+        )}
+      </div>
+      <div>
+        <p className="text-lg font-bold tracking-wide uppercase">
+          {ingredient.name}
+        </p>
+        <p className="text-sm text-gray-500">{ingredient.measure}</p>
+      </div>
+    </div>
+  );
+};
